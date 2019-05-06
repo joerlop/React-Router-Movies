5f4b1cf646d1d9ac63722ca8216f2ed2dd60b6b6
joerlop <jj.erlich155@gmail.com>
2019-05-06 15:44:26 -0500

    Stretch: Added NavLinks to saved movies

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink, Link } from "react-router-dom";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,9 +12,15 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <NavLink 
+            to={`/movies/${movie.id}`}
+            activeStyle={{
+              fontWeight: "bold",
+              color: "red"
+            }}
+          >
             <span className="saved-movie">{movie.title}</span>
-          </Link>
+          </NavLink>
         ))}
         <Link to="/">
           <div className="home-button">Home</div>
